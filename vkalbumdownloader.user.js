08468e40ff66034dcc397bfb058f5ff4b8ddd2ec
eag1e <eag1e3081@gmail.com>
2016-08-02 13:36:20 +0900

    Update vkalbumdownloader.user.js

--- vkalbumdownloader.user.js
+++ vkalbumdownloader.user.js
@@ -1,4 +1,7 @@
-//Copyright (c) 2016 e@g1e http://otamil.militaryblog.jp/
+//Copyright (c) 2016 e@g1e https://github.com/eag1e
+// 2016/08/01 e@g1e modified for the new VK album design.
+// 2016/08/02 e@g1e modified for the Chrome + Tampermonkey.
+//
 //Released under the MIT license
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -13,8 +16,9 @@
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // USES https://raw.github.com/vuplea/tiny_zip_js
 // ==UserScript==
-// @name          VK album downloader + direct HiRes links
-// @description   This script introduces in VK.com album pages the ability to relink all photos to the direct full resolution link, and to also automatically create a .zip of the photos for quick download..
+// @name          VK album downloader
+// @autho         eag1e
+// @description   This script introduces in VK.com album pages the ability to automatically create a .zip of the photos for quick download.
 // @include       http://*vk.com/*
 // @include       https://*vk.com/*
 // @grant         GM_getValue
@@ -22,47 +26,47 @@
 // @grant         GM_xmlhttpRequest
 // @version       0.4 RC
 // @require       http://raw.github.com/vuplea/tiny_zip_js/master/tiny_zip.js
-// @downloadURL   https://github.com/eag1e/VK_album_downloader/raw/master/vkalbumdownloader.user.js
+// @downloadURL   http://raw.github.com/eag1e/VK_album_downloader/master/vkalbumdownloader.user.js
 // ==/UserScript==
 
 
 
-if (GM_getValue("switch") == undefined) GM_setValue("switch", "on");
+//if (GM_getValue("switch") === undefined) GM_setValue("switch", "on");
 //
 var nPhotos, album_box, cur_done = Infinity;
 var anchors = document.getElementsByClassName("photos_row ");
 //
 
+//起動ループ
+var loop = setInterval(launcher, 500);
 
+//アルバム判定
 function is_album()
-{//alert(anchors.length);
- //alert(anchors[0].getElementsByTagName('a')[0].href);
-	return anchors.length > 0 && anchors[0].getElementsByTagName('a')[0].href != undefined;
+{
+// alert(document.getElementsByClassName("photos_album_page")[0]);
+// alert(anchors.length);
+// alert(anchors[0].getElementsByTagName('a')[0].href);
+	return document.getElementsByClassName("photos_album_page")[0] !== undefined && anchors.length > 0 && anchors[0].getElementsByTagName('a')[0].href !== undefined;
 }
 
 var reverse;
 function launcher()
 {//alert(is_album());
-	if (is_album())
-	{
-		if (document.getElementById("vuplea_vk_album") == undefined)
-		{
+	if (is_album()) {
+		if (document.getElementById("vuplea_vk_album") === null) {
 			if (anchors[0].getElementsByTagName('a')[0].href.indexOf("?rev=1") != -1)
 				reverse = true;
 			else
 				reverse = false;
 			setTimeout(main, 100);
 		}
-		if (GM_getValue("switch") == "on") setTimeout(update_links, 200);
+		setTimeout(update_links, 200);
 	}
 }
-var loop = setInterval(launcher, 500);
 
-function update_links()
-{
+function update_links() {
 	if (!is_album()) return;
-	for (var i = cur_done; i < anchors.length; i += splits.length)
-	{
+	for (var i = cur_done; i < anchors.length; i += splits.length) {
 		var splits = get_splits(createUrl(i));
 		for (var j = 0; j < splits.length && i+j < anchors.length; ++j)
 			anchors[i+j].getElementsByTagName('a')[0].href = findLink(splits[j]);
@@ -70,41 +74,39 @@ function update_links()
 	cur_done = anchors.length;
 }
 
-function createUrl(i)
-{
-	return "al_photos.php?act=show&al=1&list=" 
+function createUrl(i) {
+	return "al_photos.php?act=show&al=1&list="
 	+ document.URL.split("/")[3].match(/[a-z\-0-9_]+/)[0]
 	+ (reverse ? "%2Frev" : "") //reverse
 	+ "&offset="
 	+ i;
 }
 
-function main()
-{
+function main() {
 	if (!is_album()) return;
 	anchors = document.getElementsByClassName("photos_row ");
 	nPhotos = document.getElementsByClassName("ui_crumb_count")[0].textContent.match(/[0-9]+/)[0];
 	album_box = document.getElementsByClassName("photos_album_page")[0];
-	if (album_box == undefined) album_box = document.getElementsByClassName("photos_tag_page")[0];
+	if (album_box === undefined) album_box = document.getElementsByClassName("photos_tag_page")[0];
 	cur_done = 0;
 	//
 	var text = document.createElement("TD");
 	text.innerHTML = "VK album +  ";
 	//
-	relinkB = document.createElement("INPUT");
-	relinkB.type = "button";
-	set_switch(relinkB, GM_getValue("switch"));
+//	relinkB = document.createElement("INPUT");
+//	relinkB.type = "button";
+//	set_switch(relinkB, GM_getValue("switch"));
 	//
-	scrollB = document.createElement("INPUT");
-	scrollB.type = "button";
-	scrollB.value = "Scroll";
-	var scroll_launcher = function()
-	{
-		scrollB.disabled = "disabled";
-		clearInterval(loop);
-		scroll();
-	};
-	scrollB.addEventListener("click", scroll_launcher);
+//	scrollB = document.createElement("INPUT");
+//	scrollB.type = "button";
+//	scrollB.value = "Scroll";
+//	var scroll_launcher = function()
+//	{
+//		scrollB.disabled = "disabled";
+//		clearInterval(loop);
+//		scroll();
+//	};
+//	scrollB.addEventListener("click", scroll_launcher);
 	//
 	zipB = document.createElement("INPUT");
 	zipB.type = "button";
@@ -112,22 +114,23 @@ function main()
 	zipB.addEventListener("click", function()
 	{
 		zipB.disabled = "disabled";
-		set_switch(relinkB, "on");
-		scroll_launcher();
+//		set_switch(relinkB, "on");
+//		scroll_launcher();
+        clearInterval(loop);
 		setTimeout(zipper, 300 + nPhotos*4);
 	});
 	//
 	var script_row = document.createElement("TR");
 	script_row.appendChild(text);
-	script_row.appendChild(document.createElement("TD").appendChild(relinkB));
-	script_row.appendChild(document.createElement("TD").appendChild(scrollB));
+//	script_row.appendChild(document.createElement("TD").appendChild(relinkB));
+//	script_row.appendChild(document.createElement("TD").appendChild(scrollB));
 	script_row.appendChild(document.createElement("TD").appendChild(zipB));
 	var script_table = document.createElement("TABLE");
 	script_table.id = "vuplea_vk_album";
 	script_table.style.cssText = "margin-left:22px; margin-top:10px; margin-bottom:-15px";
 	script_table.appendChild(script_row);
 	album_box.insertBefore(script_table, album_box.firstChild);
-	if (document.getElementById("photos_upload_area_wrap") != undefined)
+	if (document.getElementById("photos_upload_area_wrap") !== undefined)
 		document.getElementById("photos_upload_area_wrap").style.marginTop = "25px";
 }
 
@@ -168,6 +171,7 @@ function findLink (mess)
 	return sources[imax].replace(/\\\//g, "/");
 }
 
+/*
 function set_switch(button, state)
 {
 	if (state == "on")
@@ -185,12 +189,12 @@ function set_switch(button, state)
 		GM_setValue("switch", "off");
 		button.value = "Enable";
 		button.addEventListener("click",
-			function(){set_switch(button, "on");})
+			function(){set_switch(button, "on");});
 	}
 }
-
-function scroll()
-{
+*/
+/*
+function scroll() {
 	if (!is_album()) return;
 	if (anchors.length == nPhotos)
 	{
@@ -200,12 +204,13 @@ function scroll()
 	unsafeWindow.photos.load();
 	setTimeout(scroll, 100);
 }
+*/
 
 var album_title, prog_text, prog_bar, download_row, i, zip,album_url;
 function zipper(photo_resp)
 {
 	if (!is_album()) return;
-	if (photo_resp == undefined)
+	if (photo_resp === undefined)
 	{
 		album_title = document.title.split("|")[0].trim();
 		var href = window.location.href;
@@ -229,7 +234,6 @@ function zipper(photo_resp)
 		//
 		i = 0;
 		zip = new tiny_zip();
-		
 	}
 	else
 	{
@@ -256,7 +260,7 @@ function zipper(photo_resp)
 		overrideMimeType: "text/plain; charset=x-user-defined",
 		onload: zipper
 	});
-	i++;	
+	i++;
 	prog_text.innerHTML = i + "/" + nPhotos;
 	prog_bar.fillRect(0,0,300 * i/nPhotos, 25);
 }
